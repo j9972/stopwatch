73018209b8057fea1e87c85e9820c5789adf495b
jung su young <j9972@naver.com>
2022-09-18 17:58:42 +0900

    stopwatch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Stopwatch stopwatch = new Stopwatch();
        //  Stopwatch stopwatch2 = new Stopwatch(); -> 스탑워치 2개 만들 수 있음
+        //  Stopwatch stopwatch3 = new Stopwatch(); -> 스탑워치 3개 만들 수 있음
     }
 }
